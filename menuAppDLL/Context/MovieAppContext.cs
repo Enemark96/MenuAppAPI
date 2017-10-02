340745671dd5879b04c4c75f9bafdba72165a70b
Enemark96 <je.enemark96@gmail.com>
2017-10-02 12:44:35 +0200

    changes
    changes

--- menuAppDLL/Context/MovieAppContext.cs
+++ menuAppDLL/Context/MovieAppContext.cs
@@ -12,11 +12,20 @@ namespace menuAppDAL.Context
             new DbContextOptionsBuilder<MovieAppContext>().UseInMemoryDatabase("TheDB").Options;
 
         //Options That we want in Memory
-        public MovieAppContext() : base(options)
-        {
-            
-        }
+        //public MovieAppContext() : base(options)
+        //{
 
+        //}
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(
+                    @"Server=tcp:jaco6143.database.windows.net,1433;Initial Catalog=MenuAPP;Persist Security Info=False;User ID=jaco6143;Password=Jacob123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            }
+        }
         public DbSet<Movie> Movies { get; set; }
         public DbSet<Rental> Rentals { get; set; }
 
